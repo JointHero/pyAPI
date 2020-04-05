38aa6b94ca1804a5d871ecaa89010e9e534a82bf
ZhangJun <ibmzhangjun@139.com>
2020-04-05 08:29:03 +0000

    1.1.2

--- util/genpwd.py
+++ util/genpwd.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 from passlib.context import CryptContext
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def get_password_hash(password):
     return pwd_context.hash(password)
